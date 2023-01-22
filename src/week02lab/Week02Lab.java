81d6777d6f85b4f65b6894df797c99594848fafe
allen <allen@10.0.0.118>
2023-01-22 12:39:11 -0700

    Added a println to say "Hello"

--- src/week02lab/Week02Lab.java
+++ src/week02lab/Week02Lab.java
@@ -16,6 +16,9 @@ public class Week02Lab {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello");
+        
+        
     }
     
 }
